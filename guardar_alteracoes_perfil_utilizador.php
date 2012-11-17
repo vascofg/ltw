d1610fdddb9e44b47658406554d89db4bb5e8cbb
Maria <MJ@ei09034.vpn.fe.up.pt>
2012-11-17 04:33:53 +0000

    Editar perfil de utilizador implementado (com algumas melhorias ainda a fazer)
    Implementado a edição tanto para utilizadores e editores, como para
    administradores, que podem editar os dados de qualquer utilizador.

--- guardar_alteracoes_perfil_utilizador.php
+++ guardar_alteracoes_perfil_utilizador.php
@@ -4,9 +4,69 @@
 	require_once 'db/db.php'; //in this file it's needed either way
 	if(!isset($_SESSION['username']) || $_SESSION['user_type']<2) //if not logged in or not admin, go away
 		redirectmsg("./", 0);
-	$id=$_POST['id'];
-
+	$username=$_GET['username'];
+	$user_type=$_POST['user_type'];
+	$pass_actual=$_POST['pass_actual'];
+		
+	$nova_pass=$_POST['nova_pass'];
+	$nova_pass_2=$_POST['nova_pass_2'];
+	
+	// if the user isn't an administrator, his current password needs to be verified
+	if($_SESSION['user_type'] != 2)
+	{
+	
+	$password=crypt($username.$pass_actual, '$1$'.substr(md5($pass_actual.$username), 0, 8)); //le awesome salt
+	$stmt = $db->prepare('SELECT rowid, username, user_type FROM user WHERE username = :username and password=:password');
+	$stmt->bindparam(':username', $username);
+	$stmt->bindparam(':password', $password);
+	}
+	// if the user is an administrator, he doesn't need to input the current password of the user
+	else
+	{
+	$stmt = $db->prepare('SELECT rowid, username, user_type FROM user WHERE username = :username ');
+	$stmt->bindparam(':username', $username);
+	}
+	
+	$stmt->execute();
+	$stmt = $stmt->fetchAll();
+	
+	if(count($stmt)==0){ //if no results
+		echo "<p>Os dados não foram alterados.</p>";
+		echo "<p>Nenhum utilizador encontrado</p>";
+	}
+	else {
+		
+		$row=$stmt[0];
+		
+		if($nova_pass != $nova_pass_2)
+		{
+			echo "<p>Os dados não foram alterados.</p>";
+			
+			echo "<p>A nova password não foi bem confirmada!</p>";
+		}	
+		else
+		{
+			$password=crypt($username.$nova_pass, '$1$'.substr(md5($nova_pass.$username), 0, 8)); //le awesome salt
+			$stmt = $db->prepare('UPDATE user SET user_type = :user_type, password =:nova_pass WHERE username = :username');
+			$stmt->bindparam(':user_type', $user_type);
+			
+			if(empty($nova_pass) || empty($nova_pass_2))
+			{
+				$stmt->bindparam(':nova_pass', $pass_actual);
+			}
+			else			
+			{
+				$stmt->bindparam(':nova_pass', $password);
+			}
+			
+			$stmt->bindparam(':username', $username);
+			
+		}	
+		
+	}
 ?>
+
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -28,6 +88,19 @@
 			</ul>
 		</div>
 		<div id="conteudo">
+		<?
+		
+		if(! $stmt->execute())
+			{
+				echo "<p>A edição de dados falhou!</p>";
+			}
+			else
+			{
+				echo "<p>Perfil editado com sucesso!</p>";
+			}
+		
+		?>
+		
 		</div>
 		<div id="rodape">
 			<p>Projecto 1 de LTW @ FEUP - 2012</p>
